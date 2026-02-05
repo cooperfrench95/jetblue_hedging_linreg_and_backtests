9a8d1f0dd35dba3885ae5a88dc180dc5491da819
Cooper French <cooperfrench95@gmail.com>
2026-02-05 17:52:30 +0700

    init project, implement basic linalg functions

---
+++ src/main.rs
@@ -0,0 +1,67 @@
+use std;
+
+type SimpleVector = [f32; 2];
+
+type TwoByTwoMatrix = [SimpleVector; 2];
+
+fn getDeterminant(matrix: &TwoByTwoMatrix) -> f32 {
+    let a = matrix[0][0];
+    let b = matrix[0][1];
+    let c = matrix[1][0];
+    let d = matrix[0][1];
+
+    (a * d) - (b * c)
+}
+
+fn invert(matrix: &TwoByTwoMatrix) -> TwoByTwoMatrix {
+    let determinant: f32 = getDeterminant(&matrix);
+    let negOne = -1f32;
+
+    let negatedForm: TwoByTwoMatrix = [
+        [matrix[1][1], negOne * matrix[1][0]],
+        [negOne * matrix[1][1], matrix[1][0]],
+    ];
+
+    let inverted = multiply_matrix_by_scalar(&negatedForm, 1f32 / determinant);
+    return inverted;
+}
+
+fn multiply_matrix_by_vector(matrix: &TwoByTwoMatrix, vec: &SimpleVector) -> SimpleVector {
+    let mut outputVec: Vec<f32> = Vec::with_capacity(2);
+
+    outputVec.push((matrix[0][0] * vec[0]) + (matrix[0][1] * vec[1]));
+    outputVec.push((matrix[1][0] * vec[0]) + (matrix[1][1] * vec[1]));
+
+    return [outputVec[0], outputVec[1]];
+}
+
+fn multiply_matrix_by_scalar(matrix: &TwoByTwoMatrix, scalar: f32) -> TwoByTwoMatrix {
+    let mut outputVecOne: Vec<f32> = Vec::with_capacity(2);
+    let mut outputVecTwo: Vec<f32> = Vec::with_capacity(2);
+
+    outputVecOne.push(matrix[0][0] * scalar);
+    outputVecTwo.push(matrix[0][1] * scalar);
+    outputVecOne.push(matrix[1][0] * scalar);
+    outputVecOne.push(matrix[1][1] * scalar);
+
+    return [
+        [outputVecOne[0], outputVecOne[1]],
+        [outputVecTwo[0], outputVecTwo[1]],
+    ];
+}
+
+fn multiply_matrix_by_matrix(
+    matrixOne: &TwoByTwoMatrix,
+    matrixTwo: &TwoByTwoMatrix,
+) -> TwoByTwoMatrix {
+    let a = (matrixOne[0][0] * matrixTwo[0][0]) + (matrixOne[0][1] * matrixTwo[1][0]);
+    let b = (matrixOne[0][0] * matrixTwo[0][1]) + (matrixOne[0][1] * matrixTwo[1][1]);
+    let c = (matrixOne[1][0] * matrixTwo[0][0]) + (matrixOne[1][1] * matrixTwo[1][0]);
+    let d = (matrixOne[1][0] * matrixTwo[0][1]) + (matrixOne[1][1] * matrixTwo[1][1]);
+
+    return [[a, b], [c, d]];
+}
+
+fn main() {
+    println!("Hello, world!");
+}
